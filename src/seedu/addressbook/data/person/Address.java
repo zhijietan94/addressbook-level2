b4c57af1a06719dc1fada6c6a7de4e5655e9f607
Zhi Jie <zhijietan94@gmail.com>
2016-08-28 21:26:45 +0800

    Add Block, Unit, Street, Postal Code classes in AddressBook.java Remove value in Address Class Encapsulate ADDRESS_VALIDATION_REGEX in Address class Add getAddress() method to return formatted String representation of address Add runtests.sh

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,11 +8,16 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
-
-    public final String value;
+    public static final String EXAMPLE = "a/123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Enter Person address in the "
+    	+ "following format: a/BLOCK, STREET, UNIT, POSTAL_CODE";
+    private static final String ADDRESS_VALIDATION_REGEX = ".+";
+    
+    private Block blockNumber;
+    private Street streetName;
+    private Unit unitNumber;
+    private PostCode postalCodeNumber;
+    
     private boolean isPrivate;
 
     /**
@@ -25,7 +30,39 @@ public class Address {
         if (!isValidAddress(address)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = address;
+        createAddress(address);
+    }
+
+    private void createAddress(String address) {
+        //TODO: Set block, Street, unit, postcode
+    }
+
+    /**
+     * @return the blockNumber
+     */
+    private String getBlock() {
+	return blockNumber.getBlock();
+    }
+
+    /**
+     * @return the streetName
+     */
+    private String getStreet() {
+        return streetName.getStreet();
+    }
+
+    /**
+     * @return the unitNumber
+     */
+    private String getUnit() {
+        return unitNumber.getUnit();
+    }
+
+    /**
+     * @return the postalCodeNumber
+     */
+    private String getPostCode() {
+        return postalCodeNumber.getPostCode();
     }
 
     /**
@@ -37,22 +74,98 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return "" + getBlock() + ", " + getStreet() + ", " + getUnit() + ", " + getPostCode();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.getAddress().equals(((Address) other).getAddress())); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return getAddress().hashCode();
+    }
+
+//    public String getAddress(){
+//	return toString();
+//    }
+    
+    /**
+     * @return the address formatted by BLOCK, STREET, UNIT, POSTAL_CODE 
+     */
+    public String getAddress() {
+	return this.toString();
     }
 
     public boolean isPrivate() {
         return isPrivate;
     }
 }
+
+/**
+ * Represents the block number in a Person's address in the address book.
+ */
+class Block{
+    private String _blockNum;
+    
+    public Block(String blockNum){
+	_blockNum = blockNum;
+    }
+    
+    public String getBlock(){
+	return _blockNum;
+    }
+    
+}
+
+/**
+ * Represents the street name in a Person's address in the address book.
+ */
+class Street{
+    private String _street;
+    
+    public Street(String street){
+	_street = street;
+    }
+    
+    public String getStreet(){
+	return _street;
+    }
+    
+}
+
+/**
+ * Represents the unit number in a Person's address in the address book.
+ */
+class Unit{
+    private String _unitNum;
+    
+    public Unit(String unitNum){
+	_unitNum = unitNum;
+    }
+    
+    public String getUnit(){
+	return _unitNum;
+    }
+    
+}
+
+/**
+ * Represents the postal code in a Person's address in the address book.
+ */
+class PostCode{
+private String _postalNum;
+    
+    public PostCode(String postalNum){
+	_postalNum = postalNum;
+    }
+    
+    public String getPostCode(){
+	return _postalNum;
+    }
+    
+}
+
