f6852e6fdda6afa18cf23547e1fc6b86a99b3b93
Zhi Jie <zhijietan94@gmail.com>
2016-08-30 22:59:16 +0800

    Update getIndexedListForViewing() in TextUi.java to use addIndexToList() from Formatter class

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,10 +1,30 @@
 package seedu.addressbook.ui;
 
+import java.util.List;
+
 class Formatter {
 
     public static String addLinePrefixAndLineSeparator(String lineSeparator, String linePrefix, String message){
         return linePrefix + message.replace("\n", lineSeparator + linePrefix);
     }
+
+    public static String addIndexToList(List<String> listItems, int displayedIndexOffset, String messageIndexedListItem) {
+      final StringBuilder formatted = new StringBuilder();
+      int displayIndex = 0 + displayedIndexOffset;
+      for (String listItem : listItems) {
+          formatted.append(getIndexedListItem(displayIndex, listItem, messageIndexedListItem)).append("\n");
+          displayIndex++;
+      }
+      return formatted.toString();
+    }
     
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem, String messageIndexedListItem) {
+        return String.format(messageIndexedListItem, visibleIndex, listItem);
+    }
     
 }
--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -148,22 +148,7 @@ public class TextUi {
 
     /** Formats a list of strings as a viewable indexed list. */
     private static String getIndexedListForViewing(List<String> listItems) {
-        final StringBuilder formatted = new StringBuilder();
-        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
-        for (String listItem : listItems) {
-            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
-            displayIndex++;
-        }
-        return formatted.toString();
-    }
-
-    /**
-     * Formats a string as a viewable indexed list item.
-     *
-     * @param visibleIndex visible index for this listing
-     */
-    private static String getIndexedListItem(int visibleIndex, String listItem) {
-        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+        return Formatter.addIndexToList(listItems, DISPLAYED_INDEX_OFFSET, MESSAGE_INDEXED_LIST_ITEM);
     }
 
 }
