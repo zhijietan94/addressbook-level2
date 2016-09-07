77f5afec2d905cd9116014ef5608e9fed437cb47
Zhi Jie <zhijietan94@gmail.com>
2016-09-07 09:57:51 +0800

    Create UtilsTest jUnit test

---
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UtilsTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
