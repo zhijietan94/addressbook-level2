e12e8686373b77d1fc6eeebe73e098f68c708189
Zhi Jie <zhijietan94@gmail.com>
2016-09-08 09:27:03 +0800

    Update UtilsTest to test methods from Utils class

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -2,13 +2,38 @@ package seedu.addressbook.common;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class UtilsTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void null_object_returns_true() {
+        String nullObject = null;
+        assertTrue(Utils.isAnyNull(nullObject));
     }
 
+    @Test
+    public void non_null_object_returns_false() {
+        String helloWorld = "Hello World!";
+        assertFalse(Utils.isAnyNull(helloWorld));
+    }
+    
+    @Test
+    public void unique_elements_returns_true() {
+        LinkedList<Integer> listOfNum = new LinkedList<>();
+        listOfNum.add(1);
+        listOfNum.add(2);
+        assertTrue(Utils.elementsAreUnique(listOfNum));
+    }
+    
+    @Test
+    public void non_unique_elements_returns_false() {
+        LinkedList<Integer> listOfNum = new LinkedList<>();
+        listOfNum.add(1);
+        listOfNum.add(1);
+        assertFalse(Utils.elementsAreUnique(listOfNum));
+    }
+    
 }
